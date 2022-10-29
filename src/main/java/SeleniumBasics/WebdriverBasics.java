46d34f9149200b52472f3eb26804ee4c95211287
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-10-29 18:01:24 +0530

    Commit
    Commit

--- src/main/java/SeleniumBasics/WebdriverBasics.java
+++ src/main/java/SeleniumBasics/WebdriverBasics.java
@@ -3,14 +3,21 @@ package SeleniumBasics;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WebdriverBasics
 {
 
 	public static void main(String[] args)
 	{
-		System.out.println(System.getProperty("user.dir"));
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/resources/chromedriver");
 		
+		//Windows
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\resources\\chromedriver.exe");
+		
+		//Mac
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/resources/chromedriver");
+		
+		WebDriverManager.chromedriver().setup();
 		
 		WebDriver driver = new ChromeDriver();
 		
