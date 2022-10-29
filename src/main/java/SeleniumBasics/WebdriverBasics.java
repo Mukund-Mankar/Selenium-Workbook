26603cce3af4736275d582bc504252f3b06b80d4
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-10-29 19:09:30 +0530

    commit
    commit

--- src/main/java/SeleniumBasics/WebdriverBasics.java
+++ src/main/java/SeleniumBasics/WebdriverBasics.java
@@ -1,3 +1,21 @@
+// Webdriver API
+	// Interface: SearchContext  [Methods declared: FindElement(), FindElements()]
+	// Interface: WebDriver extends SearchContext [Methods declared: get(), click(), sendKeys(), close()] etc.
+	// Class: "RemoteWebDriver implements WebDriver
+		// All the methods of WebDriver interface is implemented in RemoteWebDriver
+		// RemoteWebDriver class can execute WebDriver commands on a browser running on a different (remote) machine
+		// RemoteWebDriver driver = new ChromeDriver(); can also be used against WebDriver driver = new ChromeDriver(); [Will work 100%]"
+	// ChromiumDriver(class) extend RemoteWebDriver, ChromeDriver(class) extend ChromiumDriver
+	// FirefoxDriver(class) extends RemoteWebDriver
+
+// [Error] org.openqa.selenium.SessionNotCreatedException
+	// Reason
+		// Chromedriver and chrome browser version are mismatched
+
+// All the messages printed in console is not error messages but Info.
+
+
+
 package SeleniumBasics;
 
 import org.openqa.selenium.WebDriver;
@@ -19,9 +37,47 @@ public class WebdriverBasics
 		
 		WebDriverManager.chromedriver().setup();
 		
-		WebDriver driver = new ChromeDriver();
+		// Top Casting
+			// Advantage: Use same reference variable to launch different browsers
+		// Child class object is referred by parent interface reference variable
+		// As the object of "ChromeDriver()" class is created, it's constructor also gets called.
+			// The logic to launch the browser is written in the class constructor
+		WebDriver driver = new ChromeDriver(); // Launch chrome browser
+		
+		
+		//[Error] org.openqa.selenium.InvalidArgumentException
+		// It is compulsory to pass "http" or "https"
+		//driver.get("www.google.com/");
+		
+		// [Remove www] Works properly
+		//driver.get("https://google.com/");
 		
 		driver.get("https://www.google.com/");
+		
+		// Validation point
+		String pageTitle = driver.getTitle();
+		if(pageTitle.equals("Google"))
+		{
+			System.out.println("Pass -- Correct Title");
+		}
+		else
+		{
+			System.out.println("Fail -- Incorrect Title");
+		}
+		
+		// Validation point
+		String currentURL = driver.getCurrentUrl();
+		if(currentURL.equals("https://www.google.com/"))
+		{
+			System.out.println("Pass -- Correct URL");
+		}
+		else
+		{
+			System.out.println("Fail -- Incorrect URL");
+		}
+		
+		// Quit browser
+		driver.quit();
 	}
 
 }
