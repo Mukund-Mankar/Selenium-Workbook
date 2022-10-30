fa8ac96297b794d171dc5349d8e23b18b9783541
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-10-30 14:54:43 +0530

    Commit
    Commit

--- src/main/java/SeleniumBasics/S_06_NavigationMethods.java
+++ src/main/java/SeleniumBasics/S_06_NavigationMethods.java
@@ -17,15 +17,22 @@ public class S_06_NavigationMethods
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
 		
+		// This method's functionality is exactly same as driver.get() method functionality
+			// to() method internally calls driver.get() method only
+		// driver.navigate().to("https://www.amazon.in/"); is same as driver.get("https://www.amazon.in/");
+		// Open the new link in the same tab
 		driver.navigate().to("https://www.amazon.in/");
+		
 		System.out.println(driver.getTitle());
 		
 		// Simulate browser back button
 		driver.navigate().back();
+		
 		System.out.println(driver.getTitle());
 		
 		// Simulate browser forward button
 		driver.navigate().forward();
+		
 		System.out.println(driver.getTitle());
 		
 		// Simulate browser refresh button
---
+++ src/main/java/SeleniumBasics/S_07_HeadlessBrowser.java
@@ -0,0 +1,12 @@
+package SeleniumBasics;
+
+public class S_07_HeadlessBrowser
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
