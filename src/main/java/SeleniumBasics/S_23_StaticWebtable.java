bee99a8f2d415267f88f2374aaaac11f0da71759
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-06 23:14:24 +0530

    Commit
    Commit

--- src/main/java/SeleniumBasics/S_23_StaticWebtable.java
+++ src/main/java/SeleniumBasics/S_23_StaticWebtable.java
@@ -1,10 +1,13 @@
 // Static web table
-// Task: Print all company names
+// Task - 01: Print all company names
+// Task - 02: If the company name is "Island Trading" then print it's entire row
+// Task - 03: Iterate through every element of webtable
 
 package SeleniumBasics;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,6 +19,10 @@ public class S_23_StaticWebtable
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		
+		String companyNameString = "Island Trading";
+		WebElement companyRowWebElement = null;
+		
 				
 		driver.manage().window().maximize();
 		
@@ -23,8 +30,35 @@ public class S_23_StaticWebtable
 		
 		driver.get("https://www.w3schools.com/html/html_tables.asp");
 		
+		// Task - 01
 		By companyNamesBy = By.xpath("//table[@class='ws-table-all']//tr/td[1]");
 		elementUtil.printAllStringElements(elementUtil.getElementsText(companyNamesBy));
+		
+		System.out.println();
+		
+		// Task - 02
+		// Get the parent element
+		for (WebElement webElement : elementUtil.getElements(companyNamesBy))
+		{
+			if(webElement.getText().trim().toLowerCase().equals(companyNameString.trim().toLowerCase()))
+			{
+				companyRowWebElement = webElement.findElement(By.xpath(".."));
+			}
+		}
+		
+		// Print all children of parent element
+		for (WebElement webElement : companyRowWebElement.findElements(By.xpath("td")))
+		{
+			System.out.println(webElement.getText());
+		}
+		
+		System.out.println();
+		
+		// Task - 03
+		for (WebElement webElement : driver.findElements(By.xpath("//*[@id=\"customers\"]/tbody/tr/td")))
+		{
+			System.out.println(webElement.getText());
+		}
 	}
 
 }
