d1cb0c538aa6369adb1b7c90b9c6619bf49314ed
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-07 12:43:09 +0530

    Commit
    Commit

---
+++ src/main/java/SeleniumBasics/S_24_Cricinfo_XPath.java
@@ -0,0 +1,31 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class S_24_Cricinfo_XPath
+{
+	public static void main(String[] args)
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+				
+		driver.manage().window().maximize();
+		
+		GenericUtilities.ElementUtil elementUtil = new GenericUtilities.ElementUtil(driver);
+		
+		driver.get("https://www.cricbuzz.com/live-cricket-scorecard/43096/ind-vs-ned-23rd-match-super-12-group-2-icc-mens-t20-world-cup-2022");
+		
+		// Task: Who took the wicket of KL Rahul
+		By rahulWicketTaker = By.xpath("//div[@id='innings_1']//a[contains(text(),' Rahul')]/parent::div/following-sibling::div[1]/span");
+		System.out.println(elementUtil.getElementText(rahulWicketTaker));
+		
+		// Task: Get Kohli score card
+		By kohliStatsBy = By.xpath("//div[@id=\"innings_1\"]//a[contains(text(),'Kohli')]/parent::div/following-sibling::div");
+		System.out.println(elementUtil.getElementsText(kohliStatsBy));
+	}
+}
