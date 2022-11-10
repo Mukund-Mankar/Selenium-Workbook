43f89c7d0784c00786d58374096717d5f76388b6
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-10 17:06:55 +0530

    Commit
    Commit

--- src/main/java/SeleniumBasics/S_44_ClickOnWebElement.java
+++ src/main/java/SeleniumBasics/S_44_ClickOnWebElement.java
@@ -1,10 +1,53 @@
+// Different methods to perform click on the web element
+
 package SeleniumBasics;
 
-public class S_44_ClickOnWebElement {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import GenericUtilities.WaitUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class S_44_ClickOnWebElement
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.rediff.com/");
+		
+		GenericUtilities.ElementUtil elementUtil = new GenericUtilities.ElementUtil(driver);
+		GenericUtilities.ActionsUtil actionsUtil = new GenericUtilities.ActionsUtil(driver);
+		GenericUtilities.JavascriptUtil javascriptUtil = new GenericUtilities.JavascriptUtil(driver);
+		GenericUtilities.WaitUtils waitUtils = new WaitUtils(driver);
+		
+		By signInLinkBy = By.xpath("//a[text()='Sign in']");
+		By homePageLinkBy = By.xpath("//a[text()='rediff.com']");
+		
+		// Driver - Click
+		elementUtil.doClick(signInLinkBy);
+		
+		Thread.sleep(2000);
+		
+		// Actions - Click
+		actionsUtil.doClick(homePageLinkBy);
+		
+		Thread.sleep(2000);
+		
+		// Actions - Then MoveToElement - Then Click
+		actionsUtil.doMoveToElement(signInLinkBy);
+		actionsUtil.doClick(signInLinkBy);
+		
+		// Javascript - Click
+		javascriptUtil.doClick(elementUtil.getElement(homePageLinkBy));
+		
+		// Wait for element - Click
+		waitUtils.ifElementVisibleAndClickable(signInLinkBy, 2);
+		elementUtil.doClick(signInLinkBy);
 	}
-
 }
