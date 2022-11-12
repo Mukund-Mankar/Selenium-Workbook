4231c69ef7c08f444b1986625d8574c62410f9a8
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-12 11:24:05 +0530

    Commit
    Commit

--- src/main/java/SeleniumBasics/S_49_XPathForSVGElement.java
+++ src/main/java/SeleniumBasics/S_49_XPathForSVGElement.java
@@ -1,47 +1,42 @@
 // SVG - Scalar Vector Graphs
+	// A vector based graphic/ icon in the webpage
+// The XPath for an SVG element we get from Chrome Dev Tools is incorrect
 // SVG elements have a tag <svg> in DOM
 // Special XPath will work with svg elements
 // Example
 	// Flipkart: Search icon to the right of input search box
-
+		// flipkart search icon: //*[local-name()='svg']/*[name()='g' and @fill-rule='evenodd']   
 
 package SeleniumBasics;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class S_49_XPathForSVGElement {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		//flipkart search icon: //*[local-name()='svg']/*[name()='g' and @fill-rule='evenodd']
-		
+	public static void main(String[] args) throws InterruptedException
+	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://petdiseasealerts.org/forecast-map#/");
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://testproject.io/");
+		
+		GenericUtilities.ElementUtil elementUtil = new GenericUtilities.ElementUtil(driver);
+		
 		Thread.sleep(5000);
-		String svgXpath = "//*[local-name()='svg' and @id='map-svg']//*[name()='g' and @id='regions']/*[name()='g']//*[name()='path']";
 		
-		List<WebElement> statesList = driver.findElements(By.xpath(svgXpath));
-		System.out.println(statesList.size());
-		Actions act = new Actions(driver);
-		for(WebElement e : statesList) {
-			act.moveToElement(e).perform();
-			String name = e.getAttribute("name");
-			System.out.println(name);
-				if(name.equals("Delaware")) {
-					e.click();
-					break;
-				}
-		}
+		// XPath for SVG element
+		//By playVideoSVGIcon = By.xpath("(//*[local-name()='svg'])[2]");
 		
+		// XPath for SVG child element
+		By playVideoSVGChildIconBy = By.xpath("((//*[local-name()='svg'])[2]//*[name()='path'])[2]");
+		
+		//elementUtil.doClick(playVideoSVGIcon);
+		elementUtil.doClick(playVideoSVGChildIconBy); 	
 	}
-
 }
